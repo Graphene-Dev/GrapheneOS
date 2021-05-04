195ced6484630cc808a645d5322e25e45860a92a
StealthHydra179 <aidenm888@gmail.com>
2021-05-04 13:11:24 -0400

    Update boot.asm

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -1,4 +1,5 @@
 jmp start
+
 output:
     mov ah, 0x0e 
     int 0x10
@@ -34,34 +35,5 @@ start:
     call output
     call newLine
 
-; jmp keyboard
-
-; keyboard: 
-    ; Commented this out for now
-    ;  mov ah, 0x0e 
-    ; mov al, ''
-    ; int 0x10
-    ; mov ah, 0x0e
-    ; mov al, 'i'
-    ; int 0x10
-    ; mov ah, 0x0e
-    ; mov al, 'n'
-    ; int 0x10
-    ; mov ah, 0x0e
-    ; mov al, 'p'
-    ; int 0x10
-    ; mov ah, 0x0e
-    ; mov al, 'u'
-    ; int 0x10
-    ; mov ah, 0x0e
-    ; mov al, 't'
-    ; int 0x10
-
-
-; jmp $
-
-
-
-
-times 510-($-$$) db 0 ; add  some 0 bits 
-db 0x55, 0xaa ; adding those fancy bits in the end that makes it boot 
+times 510-($-$$) db 0 
+db 0x55, 0xaa 
