6ab2184ffd3d49c6ecc001fd45de4208d7454af9
StealthHydra179 <47306082+StealthHydra179@users.noreply.github.com>
2021-05-23 21:07:45 -0400

    can type lots of things nowwwww yayay but delete doesnt work?

--- Bootloader/boot.asm
+++ Bootloader/boot.asm
@@ -37,10 +37,11 @@ start:
 
 
     ;read input
-    mov ah, 0x00
-    int 0x16
-    call output
-
+    input:
+        mov ah, 0x00
+        int 0x16
+        call output
+    jmp input
 
 times 510-($-$$) db 0 
 db 0x55, 0xaa 
